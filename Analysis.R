e8a0817802198261b74fea4052fea8b3094c5197
Adway Wadekar <adway@adway.io>
2021-03-20 00:44:54 -0400

    Add comments

--- Analysis.R
+++ Analysis.R
@@ -8,9 +8,15 @@ library(readr)
 ncs <- spss.get("./data/DS0002/20240-0002-Data.sav", use.value.labels=TRUE)
 
 vars <- c("SD2", "SD15", "SD6", "SD10A", "SD19", "SD23A", "DSM.PDS", "PD.OND", "DSM.AGO", "AGO.OND", "DSM.SP", "SP.OND", "DSM.SO", "SO.OND", "DSM.PTS", "PTS.OND", "DSM.GAD", "GAD.OND", "DSM.MDDH", "MDDH.OND", "DSM.ALA", "ALA.OND", "DSM.ALD", "ALD.OND", "DSM.DRA", "DRA.OND", "DSM.DRD", "DRD.OND")
+## Split Variables
 ncs.sub <- ncs[vars]
 
 # Suicide Attempt Recode
 ncs.sub$attempt <- NA
 ncs.sub$attempt[ncs.sub$SD2=="NO" | ncs.sub$SD15=="NO" | ncs.sub$SD5=="NO" | ncs.sub$SD19=="NO"] <- 0
 ncs.sub$attempt[ncs.sub$SD5=="YES" | ncs.sub$SD19=="YES"] <- 1
+
+## Re-split on combined/stay-the-same variables, leaving out uncombined suicide variables
+
+## Re-name variables
+
